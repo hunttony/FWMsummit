75872d4d37de4c3d3a478c047b2d0b1476f08456
hunttony <128110405+hunttony@users.noreply.github.com>
2023-10-28 08:59:37 -0500

    Update scripts.js
    revamp

--- scripts.js
+++ scripts.js
@@ -1,97 +1,17 @@
+
+
 function toggleDropdown() {
-    const dropdown = document.querySelector('.dropdown-menu');
-    if (dropdown.style.display === 'none' || dropdown.style.display === '') {
-        dropdown.style.display = 'block';
-    } else {
-        dropdown.style.display = 'none';
-    }
+  const dropdown = document.querySelector('.dropdown-menu');
+  if (dropdown.style.display === 'none' || dropdown.style.display === '') {
+      dropdown.style.display = 'block';
+  } else {
+      dropdown.style.display = 'none';
   }
+}
+
+document.getElementById("bookNow").addEventListener("click", function() {
+  window.location.href = "https://www.eventbrite.com/e/the-finance-wealth-mastery-summit-tickets-724272647987?aff=oddtdtcreator"; // Replace with your desired URL
+});
 
 
 
-const elts = {
-    text1: document.getElementById("text1"),
-    text2: document.getElementById("text2")
-  };
-  
-  const texts = [
-    "KEEP YOUR MONEY",
-    "GROW YOUR MONEY",
-    "PROTECT YOUR MONEY",
-    "GUARANTEE YOUR MONEY",
-    
-  ];
-  
-  const morphTime = 1.5;
-  const cooldownTime = 0.5;
-  
-  let textIndex = texts.length - 1;
-  let time = new Date();
-  let morph = 0;
-  let cooldown = cooldownTime;
-  
-  elts.text1.textContent = texts[textIndex % texts.length];
-  elts.text2.textContent = texts[(textIndex + 1) % texts.length];
-  
-  function doMorph() {
-    morph -= cooldown;
-    cooldown = 0;
-  
-    let fraction = morph / morphTime;
-  
-    if (fraction > 1) {
-        cooldown = cooldownTime;
-        fraction = 1;
-    }
-  
-    setMorph(fraction);
-  }
-  
-  function setMorph(fraction) {
-    elts.text2.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
-    elts.text2.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
-  
-    fraction = 1 - fraction;
-    elts.text1.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
-    elts.text1.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
-  
-    elts.text1.textContent = texts[textIndex % texts.length];
-    elts.text2.textContent = texts[(textIndex + 1) % texts.length];
-  }
-  
-  function doCooldown() {
-    morph = 0;
-  
-    elts.text2.style.filter = "";
-    elts.text2.style.opacity = "100%";
-  
-    elts.text1.style.filter = "";
-    elts.text1.style.opacity = "0%";
-  }
-  
-  function animate() {
-    requestAnimationFrame(animate);
-  
-    let newTime = new Date();
-    let shouldIncrementIndex = cooldown > 0;
-    let dt = (newTime - time) / 1000;
-    time = newTime;
-  
-    cooldown -= dt;
-  
-    if (cooldown <= 0) {
-        if (shouldIncrementIndex) {
-            textIndex++;
-        }
-  
-        doMorph();
-    } else {
-        doCooldown();
-    }
-  }
-  
-  animate();
-  
-  
-  
-  
