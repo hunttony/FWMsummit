e105b40c3bb0922f9fd8f358a66c6b81694c4cc7
hunttony <128110405+hunttony@users.noreply.github.com>
2023-10-28 22:34:20 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,13 +1,19 @@
 
 
-function toggleDropdown() {
-  const dropdown = document.querySelector('.dropdown-menu');
-  if (dropdown.style.display === 'none' || dropdown.style.display === '') {
-      dropdown.style.display = 'block';
-  } else {
-      dropdown.style.display = 'none';
-  }
-}
+document.addEventListener("DOMContentLoaded", function () {
+  const menuIcon = document.querySelector(".menu-icon");
+  const mobileMenu = document.querySelector(".mobile-menu");
+  const overlay = document.querySelector(".overlay");
+
+  menuIcon.addEventListener("click", function () {
+    document.body.classList.toggle("menu-open");
+  });
+
+  overlay.addEventListener("click", function () {
+    document.body.classList.remove("menu-open");
+  });
+});
+
 
 document.getElementById("bookNow").addEventListener("click", function() {
   window.location.href = "https://www.eventbrite.com/e/the-finance-wealth-mastery-summit-tickets-724272647987?aff=oddtdtcreator"; // Replace with your desired URL
